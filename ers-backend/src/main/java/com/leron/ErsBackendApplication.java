f35ba28e29476e1b71e52f8971c107ac050a6ba7
Leron Charles <lc443@njit.edu>
2020-10-21 17:00:07 -0400

    initialized springboot project

---
+++ ers-backend/src/main/java/com/leron/ErsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsBackendApplication.class, args);
+	}
+
+}
