08dbea2031943825728a581ddc6f8a30efbeb45b
Leron Charles <LeroniMac@Lerons-iMac.fios-router.home>
2020-12-05 16:49:13 -0500

    added components to front-end

--- ers-front-end/src/app/app-routing.module.ts
+++ ers-front-end/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './employees/login/login.component';
+import { RegisterComponent } from './employees/register/register.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
 
 const routes: Routes = [
-
+{ path: '', redirectTo: '/home', pathMatch: 'full'},
+{ path: 'home', component: HomepageComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
--- ers-front-end/src/app/app.module.ts
+++ ers-front-end/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav/nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { RegisterComponent } from './employees/register/register.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { LoginComponent } from './employees/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent,
+    RegisterComponent,
+    HomepageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
